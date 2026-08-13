6f6f105f61f1660e965970df0201fa6f99a0f3b8
finchcode-sudo <poqq26566@gmail.com>
2026-08-13 23:04:31 +0800

    Update EditActivity.kt

--- app/src/main/java/com/example/pinyinnotes/EditActivity.kt
+++ app/src/main/java/com/example/pinyinnotes/EditActivity.kt
@@ -35,7 +35,7 @@ import java.util.regex.Pattern
 class EditActivity : AppCompatActivity() {
 
     private lateinit var noteUri: Uri
-    private lateinit var editText: EditText
+    private lateinit var editText: FastEditText   // ← 唯一改动：EditText → FastEditText
     private lateinit var btnToggleMode: ImageButton
     private lateinit var btnChooseApp: ImageButton
     private lateinit var scrollReadView: ScrollView
